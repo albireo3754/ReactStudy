f82fe2781b53c2185ff7de9a74eb92efa6615782
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-09 23:56:19 +0900

    RSP HOOKS

--- RCP/src/RSP.jsx
+++ RCP/src/RSP.jsx
@@ -1,204 +1,139 @@
-import React, { useState, useRef, useEffect, memo } from "react";
+import React, { useState, useRef } from "react";
 
-const rspCoords = {
-  rock: "0px",
-  scissor: "-142px",
-  paper: "-284px",
-};
+const ResponseCheck = () => {
+  const [state, setState] = useState("wating");
+  const [message, setMessage] = useState("클릭해서 시작하세요");
+  const [result, setResult] = useState([]);
+  const timeOut = useRef(null);
+  const startTime = useRef();
+  const endTime = useRef();
 
-const scores = {
-  scissor: 1,
-  rock: 0,
-  paper: -1,
-};
+  const onClick = () => {
+    if (state === "wating") {
+      setState("ready");
+      setMessage("초록색이 되면 화면을 클릭하세요");
 
-const computerChoice = (imgCoord) => {
-  return Object.entries(rspCoords).find(function (v) {
-    return v[1] === imgCoord;
-  })[0];
-};
-
-const RSP = () => {
-  const [result, setResult] = useState("");
-  const [imgCoord, setImgCoord] = useState(0);
-  const [score, setScore] = useState(0);
-  const interval = useRef(null);
-
-  useEffect(() => {
-    /*componentDidMount, componentDidUpdate 역할 */
-    console.log(imgCoord);
-    interval.current = setInterval(changeHand, 200);
-    return () => {
-      /**componentWillUnmount 역할}**/
-      clearInterval(interval.current);
-    };
-  }, [imgCoord]);
-
-  const changeHand = () => {
-    console.log(imgCoord);
-    if (imgCoord === rspCoords.rock) {
-      setImgCoord(rspCoords.scissor);
-    } else if (imgCoord === rspCoords.scissor) {
-      setImgCoord(rspCoords.paper);
-    } else {
-      setImgCoord(rspCoords.rock);
-    }
-  };
-
-  const onClick = (choice) => () => {
-    clearInterval(interval.current);
-    const myScore = scores[choice];
-    const cpuScore = scores[computerChoice(imgCoord)];
-    const diff = myScore - cpuScore;
-    if (diff === 0) {
-      setResult("비겼습니다.");
-    } else if ([-1, 2].includes(diff)) {
-      setResult("이겼습니다.");
-      setScore((prevState) => {
-        console.log(prevState);
-        return prevState + 1;
+      timeOut.current = setTimeout(() => {
+        setState("now");
+        setMessage("지금클릭");
+      }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+      startTime.current = new Date();
+      console.log(startTime, startTime.current);
+    } else if (state === "ready") {
+      clearTimeout(timeOut.current);
+      // 성급하게 클릭
+      setState("wating");
+      setMessage("너무 성급하시군요! 초록색이 된 후에 클릭하세요.");
+    } else if (state === "now") {
+      // 반응속도 체크
+      endTime.current = new Date();
+      setState("wating");
+      setMessage("클릭해서 시작하세요");
+      setResult((prevResult) => {
+        console.log(prevResult, "prev");
+        return [...prevResult, endTime.current - startTime.current];
       });
-    } else {
-      setResult("졌습니다.");
-      setScore((prevState) => prevState - 1);
     }
-    setTimeout(() => {
-      interval.current = setInterval(changeHand, 2000);
-    }, 2000);
   };
 
+  const onReset = () => {
+    setResult([]);
+  };
+
+  const renderAverage = () => {
+    if (result.length !== 0) {
+      return (
+        <div>{`평균 시간: ${
+          result.reduce((acc, cur) => acc + cur) / result.length
+        } ms`}</div>
+      );
+    }
+  };
   return (
     <>
-      <div
-        id="computer"
-        style={{
-          background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
-        }}
-      ></div>
-      <button id="scissor" className="btn" onClick={onClick("scissor")}>
-        가위
-      </button>
-      <button id="rock" className="btn" onClick={onClick("rock")}>
-        바위
-      </button>
-      <button id="paper" className="btn" onClick={onClick("paper")}>
-        보
-      </button>
-      <div> {result} </div>
-      <div> 현재 {score} 점</div>
+      <div id="screen" className={state} onClick={onClick}>
+        {message}
+      </div>
+      {renderAverage()}
+      <button onClick={onReset}>리셋</button>
     </>
   );
 };
-// import React, { Component } from "react";
+// import React, { PureComponent } from "react";
 
-// const rspCoords = {
-//   rock: "0px",
-//   scissor: "-142px",
-//   paper: "-284px",
-// };
-
-// const scores = {
-//   scissor: 1,
-//   rock: 0,
-//   paper: -1,
-// };
-
-// const computerChoice = (imgCoord) => {
-//   return Object.entries(rspCoords).find(function (v) {
-//     return v[1] === imgCoord;
-//   })[0];
-// };
-
-// class RSP extends Component {
+// class ResponseCheck extends PureComponent {
 //   state = {
-//     result: "",
-//     imgCoord: 0,
-//     score: 0,
-//   };
-//   interval;
-
-//   componentDidMount() {
-//     this.interval = setInterval(this.changeHand, 500);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.interval);
-//   }
-
-//   changeHand = () => {
-//     const { imgCoord } = this.state;
-//     console.log("hello");
-//     if (imgCoord === rspCoords.rock) {
-//       this.setState({
-//         imgCoord: rspCoords.scissor,
-//       });
-//     } else if (imgCoord === rspCoords.scissor) {
-//       this.setState({
-//         imgCoord: rspCoords.paper,
-//       });
-//     } else {
-//       this.setState({
-//         imgCoord: rspCoords.rock,
-//       });
-//     }
+//     state: "wating",
+//     message: "클릭해서 시작하세요.",
+//     result: [],
 //   };
 
-//   onClick = (choice) => () => {
-//     clearInterval(this.interval);
-//     console.log(choice);
-//     const { imgCoord } = this.state;
-//     const myScore = scores[choice];
-//     const cpuScore = scores[computerChoice(imgCoord)];
-//     const diff = myScore - cpuScore;
-//     console.log(diff);
-//     if (diff === 0) {
+//   timeout;
+//   startTime;
+//   endTime;
+
+//   onClick = () => {
+//     const { state, message, result } = this.state;
+//     if (state === "wating") {
 //       this.setState({
-//         result: "비겼습니다.!",
+//         state: "ready",
+//         message: "초록색이 되면 화면을 클릭하세요",
 //       });
-//     } else if ([-1, 2].includes(diff)) {
+
+//       this.timeout = setTimeout(() => {
+//         this.setState({
+//           state: "now",
+//           message: "지금 클릭",
+//         });
+//       }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+//       this.startTime = new Date();
+//     } else if (state === "ready") {
+//       clearTimeout(this.timeout);
+//       // 성급하게 클릭
+//       this.setState({
+//         state: "wating",
+//         message: "너무 성급하시군요! 초록색이 된 후에 클릭하세요.",
+//       });
+//     } else if (state === "now") {
+//       // 반응속도 체크
+//       this.endTime = new Date();
 //       this.setState((prevState) => {
 //         return {
-//           result: "이겼습니다..!",
-//           score: prevState.score + 1,
-//         };
-//       });
-//     } else {
-//       this.setState((prevState) => {
-//         return {
-//           result: "졌습니다.!",
-//           score: prevState.score - 1,
+//           state: "wating",
+//           message: "클릭해서 시작하세요",
+//           result: [...prevState.result, this.endTime - this.startTime],
 //         };
 //       });
 //     }
-//     setTimeout(() => {
-//       this.interval = setInterval(this.changeHand, 500);
-//     }, 2000);
 //   };
 
+//   onReset = () => {
+//     this.setState({
+//       result: [],
+//     });
+//   };
+//   renderAverage = () => {
+//     const { result } = this.state;
+//     console.log(result);
+//     if (result.length !== 0) {
+//       return (
+//         <div>{`평균 시간: ${
+//           result.reduce((acc, cur) => acc + cur) / result.length
+//         } ms`}</div>
+//       );
+//     }
+//   };
 //   render() {
-//     const { result, score, imgCoord } = this.state;
-//     console.log(score);
 //     return (
 //       <>
-//         <div
-//           id="computer"
-//           style={{
-//             background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
-//           }}
-//         ></div>
-//         <button id="scissor" className="btn" onClick={this.onClick("scissor")}>
-//           가위
-//         </button>
-//         <button id="rock" className="btn" onClick={this.onClick("rock")}>
-//           바위
-//         </button>
-//         <button id="paper" className="btn" onClick={this.onClick("paper")}>
-//           보
-//         </button>
-//         <div> {result} </div>
-//         <div> 현재 {this.state.score}점 </div>
+//         <div id="screen" className={this.state.state} onClick={this.onClick}>
+//           {this.state.message}
+//         </div>
+//         {this.renderAverage()}
+//         <button onClick={this.onReset}>리셋</button>
 //       </>
 //     );
 //   }
 // }
-export default memo(RSP);
+
+export default ResponseCheck;
