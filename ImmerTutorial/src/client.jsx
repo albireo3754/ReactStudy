c277326f9766b4186643127a3aac30b5a4350642
starpro123 <40790219+starpro123@users.noreply.github.com>
2021-03-15 22:32:05 +0900

    no Immer

---
+++ ImmerTutorial/src/client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
+import ImmerTutorial from "./ImmerTutorial.jsx";
+
+ReactDom.render(<ImmerTutorial />, document.querySelector("#root"));
