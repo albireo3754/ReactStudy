8ba3b103ba5adb4d9cc7668aa561f32e7f4b6ec3
starpro123 <starpro123@naver.com>
2021-03-30 21:05:19 +0900

    feature: Install redux/toolkit

--- next-todo/store/index.ts
+++ next-todo/store/index.ts
@@ -1,10 +1,15 @@
 import { createWrapper, HYDRATE } from 'next-redux-wrapper';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+// import { applyMiddleware, combineReducers, createStore } from 'redux';
 import todo from './todo';
 
 const rootReducer = combineReducers({
-  todo,
+  todo: todo.reducer,
 });
+
+// const rootReducer = combineReducers({
+//   todo,
+// });
 
 const reducer = (state: any, action: any) => {
   if (action.type === HYDRATE) {
@@ -12,24 +17,31 @@ const reducer = (state: any, action: any) => {
       ...state,
       ...action.payload,
     };
+    if (state.count) nextState.count = state.count;
     return nextState;
   }
-
   return rootReducer(state, action);
 };
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-const bindMiddleware = (middleware: any) => {
-  if (process.env.NODE_ENV !== 'production') {
-    const { composeWithDevTools } = require('redux-devtools-extension');
-    return composeWithDevTools(applyMiddleware(...middleware));
-  }
-  return applyMiddleware(...middleware);
-};
+// const bindMiddleware = (middleware: any) => {
+//   if (process.env.NODE_ENV !== 'production') {
+//     const { composeWithDevTools } = require('redux-devtools-extension');
+//     return composeWithDevTools(applyMiddleware(...middleware));
+//   }
+//   return applyMiddleware(...middleware);
+// };
+
+// const initStore = () => {
+//   return createStore(reducer, bindMiddleware([]));
+// };
 
 const initStore = () => {
-  return createStore(reducer, bindMiddleware([]));
+  return configureStore({
+    reducer,
+    devTools: true,
+  });
 };
 
 export const wrapper = createWrapper(initStore);
--- next-todo/store/todo.ts
+++ next-todo/store/todo.ts
@@ -1,14 +1,15 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TodoType } from '../types/todo';
 
-export const SET_TODO_LIST = 'todo/SET_TODO_LIST';
-export const setTodo = (payload: TodoType[]) => {
-  return {
-    type: SET_TODO_LIST,
-    payload,
-  };
-};
+// export const SET_TODO_LIST = 'todo/SET_TODO_LIST';
+// export const setTodo = (payload: TodoType[]) => {
+//   return {
+//     type: SET_TODO_LIST,
+//     payload,
+//   };
+// };
 
-export const todoActions = { setTodo };
+// export const todoActions = { setTodo };
 
 interface TodoReduxState {
   todos: TodoType[];
@@ -18,13 +19,26 @@ const initialState: TodoReduxState = {
   todos: [],
 };
 
-export default function reducer(state = initialState, action: any) {
-  switch (action.type) {
-    case SET_TODO_LIST: {
-      const newState = { ...state, todos: action.payload };
-      return newState;
-    }
-    default:
-      return state;
-  }
-}
+const todo = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    setTodo(state, action: PayloadAction<TodoType[]>) {
+      state.todos = action.payload;
+    },
+  },
+});
+
+export const todoActions = { ...todo.actions };
+
+export default todo;
+// export default function reducer(state = initialState, action: any) {
+//   switch (action.type) {
+//     case SET_TODO_LIST: {
+//       const newState = { ...state, todos: action.payload };
+//       return newState;
+//     }
+//     default:
+//       return state;
+//   }
+// }
