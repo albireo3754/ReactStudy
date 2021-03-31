31be3b29d1055fbb0a2433126bb904b613b33d15
starpro123 <starpro123@naver.com>
2021-03-31 23:57:15 +0900

    feature: Make sign up design
    Make Input components

--- next-bnb/components/auth/SignUpModal.tsx
+++ next-bnb/components/auth/SignUpModal.tsx
@@ -1,14 +1,59 @@
 import styled from 'styled-components';
+import CloseXIcon from '../../public/statics/svg/auth/modal_close_x_icon.svg';
+import MailIcon from '../../public/statics/svg/auth/mail.svg';
+import PersonXIcon from '../../public/statics/svg/auth/person.svg';
+import OpenedEyeIcon from '../../public/statics/svg/auth/opend_eye.svg';
+import CloseEyeIcon from '../../public/statics/svg/auth/closed_eye.svg';
+import palette from '../../styles/palette';
+import Input from '../common/input';
+import { useState } from 'react';
 
 const Container = styled.div`
   width: 568px;
   height: 614px;
+  padding: 32px;
   background-color: white;
   z-index: 11;
+  .modal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
 `;
 
 const SignUpModal = () => {
-  return <Container>Sing Up</Container>;
+  const [hidePassword, setHidePassword] = useState(true);
+
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
+  return (
+    <Container>
+      <CloseXIcon className='modal-close-x-icon' />
+      <Input placeholder='이메일 주소' type='email' name='email' icon={<MailIcon />} />
+      <Input placeholder='성(예: 홍)' icon={<PersonXIcon />} />
+      <Input placeholder='이름(예: 길동)' icon={<PersonXIcon />} />
+      <div className='sign-up-password-input-wrapper'>
+        <Input
+          placeholder='비밀번호 설정하기'
+          type={hidePassword ? 'password' : 'text'}
+          icon={
+            hidePassword ? (
+              <CloseEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+        />
+      </div>
+    </Container>
+  );
 };
 
 export default SignUpModal;
