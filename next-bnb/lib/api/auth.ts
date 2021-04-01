992756681343fb708f0c42d6f96320981bd55ec7
starpro123 <starpro123@naver.com>
2021-04-01 23:20:45 +0900

    fix: Move auth api library pages to lib folder

---
+++ next-bnb/lib/api/auth.ts
@@ -0,0 +1,12 @@
+import axios from '../../pages/api';
+import { UserType } from '../../types/user';
+
+interface SignUpAPIBody {
+  email: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  birthday: string;
+}
+
+export const signupAPI = (body: SignUpAPIBody) => axios.post<UserType>('/api/auth/signup', body);
