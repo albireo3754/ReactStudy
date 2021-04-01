46ce6e85d7aa5451421991fa065a71ade1b29504
starpro123 <starpro123@naver.com>
2021-04-01 14:07:46 +0900

    feature: Add sign up botton component

--- next-bnb/components/auth/SignUpModal.tsx
+++ next-bnb/components/auth/SignUpModal.tsx
@@ -9,6 +9,7 @@ import Input from '../common/input';
 import React, { useState } from 'react';
 import Selector from '../common/Selector';
 import { dayList, monthList, yearList } from '../../lib/staticData';
+import Button from '../common/Button';
 
 const Container = styled.div`
   width: 568px;
@@ -52,6 +53,11 @@ const Container = styled.div`
     .sign-up-modal-birthday-year-selector {
       width: 33.333333%;
     }
+  }
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
   }
 `;
 
@@ -124,6 +130,9 @@ const SignUpModal = () => {
             disabledOptions={['년']}
           />
         </div>
+      </div>
+      <div className='sign-up-modal-submit-button-wrapper'>
+        <Button type='submit'>가입하기</Button>
       </div>
     </Container>
   );
