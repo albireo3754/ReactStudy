588f48b56caa44f8ebea0fe4285c2684a8f24c9a
starpro123 <starpro123@naver.com>
2021-05-30 20:04:33 +0900

    setting: hello snake game

---
+++ react-game/components/Snake/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div``;
+
+export interface IProps {}
+const index: React.FC = () => {
+  return <Container>hello snake game</Container>;
+};
+
+export default index;
